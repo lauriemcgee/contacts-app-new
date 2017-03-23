99fd406819706ed99d14718a792a6a41540285a6
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-23 18:58:28 -0500

    create contacts method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,15 @@ class ContactsController < ApplicationController
     return "new.html.erb"
   end
   def create
+    @contact = Contact.create(
+        first_name: params[:first_name],
+        middle_name: params[:middle_name],
+        last_name: params[:last_name],
+        bio: params[:bio],
+        phone_number: params[:phone_number,
+        email: params[:email],
+        user_id: current_user.id
+      )
     return "create.html.erb"
   end
   def show
